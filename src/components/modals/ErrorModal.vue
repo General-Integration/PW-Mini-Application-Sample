544a4915c164dd1678efdeeede0b7a23dfc3a238
CHHUONN.Sophorn <chhuonn.sophorn@ababank.com>
2023-03-02 11:34:58 +0700

    add global error modal

--- src/components/modals/ErrorModal.vue
+++ src/components/modals/ErrorModal.vue
@@ -1,7 +1,7 @@
 <template>
     <teleport to="body">
         <transition>
-            <div v-show="useError.show" class="fixed z-50 inset-0 bg-black bg-opacity-50 flex items-start justify-center items-center min-h-screen text-center" @click="backdropDismiss($event)" data-dismiss="modal">
+            <div v-show="useError.show" class="fixed z-50 inset-0 bg-black bg-opacity-50 flex items-start justify-center items-center min-h-screen text-center" @click="backdropDismiss($event)" aria-controls="dismiss">
                 <div class="bg-white rounded-md shadow-xl w-4/5 pt-4">
                     <header class="text-center font-bold text-xl">
                         <p>{{ useError.title }}</p>
@@ -10,7 +10,7 @@
                         <p>{{ useError.message }}</p>
                     </section>
                     <footer class="flex justify-center items-center p-4 border-t-2 border-gray-100">
-                        <a class="font-bold text-lg text-cyan-500" @click="buttonDismiss($event)" data-dismiss="modal">Dismiss</a>
+                        <a class="font-bold text-lg text-cyan-500" @click="buttonDismiss($event)" aria-label="dismiss">Dismiss</a>
                     </footer>
                 </div>
             </div>
@@ -27,7 +27,7 @@
     const buttonDismiss =(event)=>dismiss(event)
 
     const dismiss =(event)=>{
-        if(event.target.getAttribute('data-dismiss') === 'modal'){
+        if(event.target.getAttribute('aria-label') === 'dismiss'){
             useError.resetError();
         }
     }
