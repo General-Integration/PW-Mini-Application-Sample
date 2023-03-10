a7fdd26f4693f12a62eb506d203acb2a26308a86
CHHUONN.Sophorn <chhuonn.sophorn@ababank.com>
2023-03-10 09:26:37 +0700

    updated navication back flow

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,25 @@
 <script setup>
   import { useRouter } from 'vue-router'
   import { ErrorModal } from '@/components'
-  import { EVENTS } from '@/utils/constants'
-  import { shallowRef, onMounted } from 'vue'
   import { useErrorStore } from './stores/error'
   import { callHandler } from 'web-bridge-gateway'
+  import { shallowRef, onMounted, ref } from 'vue'
+  import { EVENTS, ROUTE_NAME } from '@/utils/constants'
   import { DefaultLayout, ChildLayout } from '@/layouts'
 
+  const fromRoute = ref();
   const router = useRouter();
   const useError = useErrorStore();
-  const isLayout = shallowRef(null);
-
+  const isLayout = shallowRef();
   // SWITCH APP LAYOUT
-  router.beforeEach((to) => {
+  router.beforeEach((to, from) => {
+    fromRoute.value = from.name;
     isLayout.value = to?.meta?.layout == 'Child' ? ChildLayout : DefaultLayout;
   })
 
   // GET BUSINESS ID GROM GET_FROFILE HANDLER 
   const handlerKey = EVENTS && EVENTS[0]['key'] ? EVENTS[0]['key'] : ''
-  
+
   onMounted(async()=>{
     if(handlerKey){
       await callHandler(handlerKey).then((res) => {
@@ -38,4 +39,12 @@
       });
     }
   })
+
+  window.addEventListener('popstate', () => {
+    if(fromRoute.value === ROUTE_NAME.eventAll){
+      router.push({ name: ROUTE_NAME.home });
+    }else if(fromRoute.value === ROUTE_NAME.home){
+      callHandler('closeApp')
+    }
+  });
 </script>
